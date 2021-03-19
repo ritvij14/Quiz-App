7411955285be49a8c58cdc2e697abc320368a9a2
UVrajSB <mryuvrajsbhadauria@gmail.com>
2021-03-19 20:13:48 +0530

    bindingApplication

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainBinding.root)
+
+        mainBinding.btnCT.setOnClickListener{
+            mainBinding.tvText.text="How's it captain"
+        }
     }
 }
