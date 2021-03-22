b1709eb103adfa8fff64aa93a4057490e787d03f
UVrajSB <mryuvrajsbhadauria@gmail.com>
2021-03-22 05:51:24 +0530

    1) Const. File    2) Main screen + Practice mode info Screen 3)adding blank activities with XML files of 10ques

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.quizapp
+
+// Constants repository
---
+++ app/src/main/java/com/example/quizapp/FourthQuestion.kt
@@ -0,0 +1,11 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FourthQuestion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fourth_question)
+    }
+}
