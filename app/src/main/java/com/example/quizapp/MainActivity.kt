72f90a2349dba9152a9efdf53afb86bcec639097
UVrajSB <mryuvrajsbhadauria@gmail.com>
2021-03-24 10:19:16 +0530

    Adding two functional questions in practice mode + added scroll bar

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.quizapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract
 import com.example.quizapp.databinding.ActivityMainBinding
-
+import com.example.quizapp.Question1
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this ,PracticeModeIntro::class.java)
             startActivity(intent)
         }
+
 
     }
 }
